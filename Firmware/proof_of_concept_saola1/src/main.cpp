5048f4b92bba0ad36112abef5df06dace9c018bf
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-02-13 17:02:33 +0000

    change LED over serial request

--- Firmware/proof_of_concept_saola1/src/main.cpp
+++ Firmware/proof_of_concept_saola1/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+#define LED 0
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("ESP Listening...");
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()){ //Listen to serial
+    String command = Serial.readStringUntil('\n'); // Reads string untill \n
+    if (command == "change")
+    {
+      Serial.println("LED change status");
+      digitalWrite(LED, !digitalRead(LED));
+    } 
+  }
 }
