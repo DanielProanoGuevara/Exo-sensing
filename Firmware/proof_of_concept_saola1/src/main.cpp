29640cc9bf52c60a3f48e865cb2a07617d7fcf33
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-02-14 11:42:21 +0000

    AT --> OK echo in serial 1

--- Firmware/proof_of_concept_saola1/src/main.cpp
+++ Firmware/proof_of_concept_saola1/src/main.cpp
@@ -1,20 +1,28 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
 
 #define LED 0
 
+// Use serial 1
+#define RXD1 18
+#define TXD1 17
+HardwareSerial BLE (1);
+
 void setup() {
+  // Start serial com
   Serial.begin(115200);
-  Serial.println("ESP Listening...");
-  pinMode(LED, OUTPUT);
+  // Start serial 1
+  BLE.begin(115200, SERIAL_8N1, RXD1, TXD1);
 }
 
 void loop() {
-  if(Serial.available()){ //Listen to serial
-    String command = Serial.readStringUntil('\n'); // Reads string untill \n
-    if (command == "change")
-    {
-      Serial.println("LED change status");
-      digitalWrite(LED, !digitalRead(LED));
-    } 
+  //Serial pass through
+  if (Serial.available()){
+    BLE.write(Serial.read());
   }
+
+  if (BLE.available()){
+    Serial.write(BLE.read());
+  }
+
 }
