3a465428fb9f44050b212a16a62c255de2a3b09c
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-04-19 12:23:14 +0100

    project ready to follow tutorial

---
+++ App/BLE_Arduino_Connect/app/src/main/java/com/example/ble_arduino_connect/presentation/Navigation.kt
@@ -0,0 +1,10 @@
+package com.example.ble_arduino_connect.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+}
