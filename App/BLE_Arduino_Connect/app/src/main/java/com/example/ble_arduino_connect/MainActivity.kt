4cbc62aa8c7aa5a305bab305c9d0f4fc7fcf199b
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-04-19 12:14:26 +0100

    initial commit for app dev

---
+++ App/BLE_Arduino_Connect/app/src/main/java/com/example/ble_arduino_connect/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.ble_arduino_connect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ble_arduino_connect.ui.theme.BLE_Arduino_ConnectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BLE_Arduino_ConnectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BLE_Arduino_ConnectTheme {
+        Greeting("Android")
+    }
+}
