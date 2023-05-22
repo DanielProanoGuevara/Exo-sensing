9274ea9b2d3a271c4773ecc94fe4010ac0c27c30
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-05-22 18:18:47 +0100

    re-generated code TIMER4, complies with datasheet, still doesnt work

--- Firmware/POC_nano_sense/Analog_Acq_BLE_33_sense/Analog_Acq_BLE_33_sense.ino
+++ Firmware/POC_nano_sense/Analog_Acq_BLE_33_sense/Analog_Acq_BLE_33_sense.ino
@@ -1,10 +1,19 @@
 #include <nrfx.h>
 #include <nrf.h>
 #include "nrf_timer.h"
-#include "nrf_gpio.h"
 
 
-volatile bool timer_triggered = false; // Flag to acces to the service function without overloading the ISR
+// Timer settings
+const unsigned long timerInterval = 1000;  // 1 second interval
+
+// Flag to track LED state
+volatile bool ledState = false;
+
+// Timer interrupt handler
+void timerInterruptHandler() {
+  ledState = !ledState;  // Toggle LED state
+}
+
 
 
 // Initialize the timer
@@ -12,68 +21,66 @@ void setupTimer(){
   // Uses timer 4, since timer 1,2,3 are used in the BLE transactions
 
   // Disable interrupts before configuring
-  //NRF_TIMER0->INTENCLR = 1UL << TIMER_INTENSET_COMPARE0_Pos;
+  NRF_TIMER4->INTENCLR = TIMER_INTENCLR_COMPARE0_Msk;
 
-  // Initialize and configure timer
-  NRF_TIMER0->TASKS_STOP = 1UL; // Stop timer in case it was running
-  NRF_TIMER0->TASKS_CLEAR = 1UL; // Clear timer to initialize it propperly 
+  // Stop and clear the timer before using it
+  NRF_TIMER4->TASKS_STOP = 1;
+  NRF_TIMER4->TASKS_CLEAR = 1;
 
-  NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_32Bit << TIMER_BITMODE_BITMODE_Pos; //16-bit mode
-  //NRF_TIMER4->MODE = 0UL; // Timer mode
-  NRF_TIMER0->PRESCALER = 4 << TIMER_PRESCALER_PRESCALER_Pos; // Prescaler: timer_freq = 16MHz / 2^prescaler = 125KHz
-  NRF_TIMER0->CC[0] = 1000; // Compare value = timer_freq * (1/PPS) -- PulsesPerSecond = 200
 
-  // Enable interrupts
-  NRF_TIMER0->INTENSET = TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos;
-  // Create a shortcut with a clear timer whenever an event occurs
-  NRF_TIMER0->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Enabled << TIMER_SHORTS_COMPARE0_CLEAR_Pos;
-
-  // Point to the interrupts vector list
-  //NVIC_EnableIRQ(TIMER0_IRQn);
+  // Set up the timer interrupt
+  NRF_TIMER4->MODE = TIMER_MODE_MODE_Timer;  // Set timer mode
+  NRF_TIMER4->BITMODE = TIMER_BITMODE_BITMODE_32Bit;  // Set timer to 32-bit mode
+  
+  // Set the timer prescaler to achieve desired interval
+  // For example, with a 16 MHz clock, prescaler 4 gives a 1 microsecond resolution
+  NRF_TIMER4->PRESCALER = 4;
+  
+  // Set the compare value to trigger the interrupt
+  // The compare value depends on the desired interval and timer resolution
+  // For example, with a 1-second interval and 1 microsecond resolution,
+  // the compare value is (timerInterval * 1000) - 1
+  NRF_TIMER4->CC[0] = (timerInterval * 1000) - 1;
+  
+  // Enable the compare interrupt on CC[0]
+  NRF_TIMER4->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
+  // Create a shortcut for the timer clearing
+  NRF_TIMER4->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
+  
+  // Enable the interrupt in the NVIC
+  NVIC_EnableIRQ(TIMER4_IRQn);
+  
   // Start the timer
-  NRF_TIMER0->TASKS_START = 1;
-}
-
-
-void TIMER0_IRQHandler(void){
-  volatile uint32_t dummy;
-  if (NRF_TIMER0->EVENTS_COMPARE[0] == 1){ // Verifies that the interrupt is triggered on the event
-    NRF_TIMER0->EVENTS_COMPARE[0] = 0; // Clears the value (in case the shortcut didn't do it)
-
-
-    dummy = NRF_TIMER0->EVENTS_COMPARE[0];
-    dummy;
-
-    //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    //timer_triggered =  true;
-  }
-}
-
-
-void toggleLed(){
-  if (timer_triggered == true){
-    // Whatever it needs to do
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    // Recovers initial flag state
-    timer_triggered = false;
-  }
+  NRF_TIMER4->TASKS_START = 1;
 }
 
 
 
 
 void setup() {
-  // Initializes the timer
-  setupTimer();
   // Initializes the debug pin
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-
-  timer_triggered = false;
-
+  // Initializes the timer
+  setupTimer();
 }
 
 void loop() {
-  toggleLed(); // Execute function served on event
-  __WFE(); // Wait for event instruction (sleeps waiting for event)
+  // Update the LED state
+  if (ledState) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  //__WFE(); // Wait for event instruction (sleeps waiting for event)
+}
+
+// Timer interrupt service routine
+void TIMER4_IRQHandler() {
+  // Check if the interrupt was triggered by CC[0]
+  if (NRF_TIMER4->EVENTS_COMPARE[0]) {
+    NRF_TIMER4->EVENTS_COMPARE[0] = 0;  // Clear the event
+    
+    timerInterruptHandler();  // Call the interrupt handler function
+  }
 }
