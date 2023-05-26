37f4f8a16b3aa5ec8e38f1aaa756db5976f92583
Daniel Proaño <dproanog@est.ups.edu.ec>
2023-05-26 13:59:11 +0100

    functions optimized to use DSP FPU functions

--- Firmware/POC_nano_sense/Analog_Acq_BLE_33_sense/Analog_Acq_BLE_33_sense.ino
+++ Firmware/POC_nano_sense/Analog_Acq_BLE_33_sense/Analog_Acq_BLE_33_sense.ino
@@ -3,6 +3,7 @@
 #include <nrf.h>
 #include "nrf_timer.h"
 #include "nrf_delay.h"
+#include "arm_math.h"
 
 #include "SPI.h"
 
@@ -13,58 +14,69 @@
 
 // Variables 
 uint8_t decimationCounter = 1;
-float FSR1_filt = 0.0f;
+float32_t FSR1_filt = 0.0f;
 uint16_t DAC_o = 0;
+
+float32_t int_res; // Auxiliar dot product result for IIR filter
+uint32_t idx; //  Auxiliar min and max index (not used)
 
 // Filter variables
 // *** IIR FSR coefficients
-float B_FSR[7] = {0.00034054f, 0.00204323f, 0.00510806f, 0.00681075f, 0.00510806f, 0.00204323f, 0.00034054f};
-float A_FSR[7] = {1.0f,        -3.5794348f, 5.65866717f,-4.96541523f, 2.52949491f,-0.70527411f, 0.08375648f};
+float32_t B_FSR[7] = {0.00034054f, 0.00204323f, 0.00510806f, 0.00681075f, 0.00510806f, 0.00204323f, 0.00034054f};
+float32_t A_FSR[7] = {1.0f,        -3.5794348f, 5.65866717f,-4.96541523f, 2.52949491f,-0.70527411f, 0.08375648f};
 
-float FSR1_x[7] = {0.0f}; // Input memory buffer
-float FSR1_y[7] = {0.0f}; // Output memory buffer
-float FSR2_x[7] = {0.0f};
-float FSR2_y[7] = {0.0f};
-float FSR3_x[7] = {0.0f};
-float FSR3_y[7] = {0.0f};
-float FSR4_x[7] = {0.0f};
-float FSR4_y[7] = {0.0f};
-float NTC1_x[7] = {0.0f};
-float NTC1_y[7] = {0.0f};
-float Fluids1_x[7] = {0.0f};
-float Fluids1_y[7] = {0.0f};
+float32_t FSR1_x[7] = {0.0f}; // Input memory buffer
+float32_t FSR1_y[7] = {0.0f}; // Output memory buffer
+float32_t FSR2_x[7] = {0.0f};
+float32_t FSR2_y[7] = {0.0f};
+float32_t FSR3_x[7] = {0.0f};
+float32_t FSR3_y[7] = {0.0f};
+float32_t FSR4_x[7] = {0.0f};
+float32_t FSR4_y[7] = {0.0f};
+float32_t NTC1_x[7] = {0.0f};
+float32_t NTC1_y[7] = {0.0f};
+float32_t Fluids1_x[7] = {0.0f};
+float32_t Fluids1_y[7] = {0.0f};
 
 // *** MA ring buffer and variables
+
 const uint8_t M = 20; // Window size
-float MA_ring_buffer_FSR1 [M] = {0.0f}; // FSR1 moving average ring buffer
-float acc_FSR1 = 0.0f; // FSR1 accumulator
-float average_FSR1 = 0.0f;
+float32_t average_FSR[4] = {0.0f};
+float32_t min_FSR[4] = {0.0f};
+float32_t max_FSR[4] = {0.0f};
+
+float32_t FSR_mean;
+float32_t FSR_min;
+float32_t FSR_max;
+
+float32_t MA_ring_buffer_FSR1 [M] = {0.0f}; // FSR1 moving average ring buffer
 uint8_t RB_i_FSR1 = 0; // FSR1 Ring buffer iterator
 
-float MA_ring_buffer_FSR2 [M] = {0.0f};
-float acc_FSR2 = 0.0f;
-float average_FSR2 = 0.0f;
+float32_t MA_ring_buffer_FSR2 [M] = {0.0f};
 uint8_t RB_i_FSR2 = 0;
 
-float MA_ring_buffer_FSR3 [M] = {0.0f};
-float acc_FSR3 = 0.0f;
-float average_FSR3 = 0.0f;
+float32_t MA_ring_buffer_FSR3 [M] = {0.0f};
 uint8_t RB_i_FSR3 = 0;
 
-float MA_ring_buffer_FSR4 [M] = {0.0f};
-float acc_FSR4 = 0.0f;
-float average_FSR4 = 0.0f;
+float32_t MA_ring_buffer_FSR4 [M] = {0.0f};
 uint8_t RB_i_FSR4 = 0;
 
-float MA_ring_buffer_NTC1 [M] = {0.0f};
-float acc_NTC1 = 0.0f;
-float average_NTC1 = 0.0f;
+float32_t MA_ring_buffer_NTC1 [M] = {0.0f};
+float32_t average_NTC1 = 0.0f;
+float32_t min_NTC1 = 0.0f;
+float32_t max_NTC1 = 0.0f;
 uint8_t RB_i_NTC1 = 0;
 
-float MA_ring_buffer_Fluids1 [M] = {0.0f};
-float acc_Fluids1 = 0.0f;
-float average_Fluids1 = 0.0f;
+float32_t MA_ring_buffer_Fluids1 [M] = {0.0f};
+float32_t average_Fluids1 = 0.0f;
+float32_t min_Fluids1 = 0.0f;
+float32_t max_Fluids1 = 0.0f;
 uint8_t RB_i_Fluids1 = 0;
+
+
+
+// Regression constants in the form y = ax^3 + bx^2 + cx + d
+
 
 // Analog acquisition
 uint16_t FSR1_raw = 0;
@@ -126,32 +138,34 @@ SPISettings mySettings(16000000, MSBFIRST, SPI_MODE0);
 
 
 /******* DSP functions ********/
-void movingAverage( float ringBuffer[],
+void movingAverage( float32_t ringBuffer[],
                     uint8_t win_size,
                     uint8_t &i,
-                    float &acc,
-                    float acq,
-                    float &average){
+                    float32_t acq,
+                    float32_t *average,
+                    float32_t *min,
+                    float32_t *max){
 
   if (i == win_size) i = 0; // Resets the iterator
-  acc -= ringBuffer[i]; // Substracts the oldest element from the accumulator
   ringBuffer[i] = acq; // Replaces the oldest element of the ring buffer with the new one
-  acc += acq; // Increses the accumulator with the new sample
-  average = acc / win_size; // Get the buffer average
+  arm_mean_f32(ringBuffer, win_size, average);
+  arm_min_f32(ringBuffer, win_size, min, &idx);
+  arm_max_no_idx_f32(ringBuffer, win_size, max);
   i++; // Move the iterator
 }
 
 void IIR( uint16_t Ain,
-          float x[],
-          float y[],
-          float A[],
-          float B[]){
+          float32_t x[],
+          float32_t y[],
+          float32_t A[],
+          float32_t B[]){
   // Convert from uint16 to voltage value
   x[0] = Ain * (3.3f / 4096.0f); // Current input value
   
   // calc. the output
+  arm_dot_prod_f32(B, x, 7, &int_res); // Intermediate calculation
   y[0] = (- A[1]*y[1] - A[2]*y[2] - A[3]*y[3] - A[4]*y[4] - A[5]*y[5] - A[6]*y[6]
-          + B[0]*x[0] + B[1]*x[1] + B[2]*x[2] + B[3]*x[3] + B[4]*x[4] + B[5]*x[5] + B[6]*x[6]);
+          + int_res);
   // Propagate inputs
   x[6] = x[5];
   x[5] = x[4];
@@ -205,19 +219,17 @@ void loop() {
 
     //IIR_filter
     IIR(FSR1_raw, 
-        (float*) &FSR1_x,
-        (float*) &FSR1_y,
+        (float32_t*) &FSR1_x,
+        (float32_t*) &FSR1_y,
         A_FSR,
         B_FSR);
 
-    IIR(FSR2_raw, (float*) &FSR2_x, (float*) &FSR2_y, A_FSR, B_FSR);
-    IIR(FSR3_raw, (float*) &FSR3_x, (float*) &FSR3_y, A_FSR, B_FSR);
-    IIR(FSR4_raw, (float*) &FSR4_x, (float*) &FSR4_y, A_FSR, B_FSR);
-    IIR(NTC1_raw, (float*) &NTC1_x, (float*) &NTC1_y, A_FSR, B_FSR);
-    IIR(Fluids1_raw, (float*) &Fluids1_x, (float*) &Fluids1_y, A_FSR, B_FSR);
+    IIR(FSR2_raw, (float32_t*) &FSR2_x, (float32_t*) &FSR2_y, A_FSR, B_FSR);
+    IIR(FSR3_raw, (float32_t*) &FSR3_x, (float32_t*) &FSR3_y, A_FSR, B_FSR);
+    IIR(FSR4_raw, (float32_t*) &FSR4_x, (float32_t*) &FSR4_y, A_FSR, B_FSR);
+    IIR(NTC1_raw, (float32_t*) &NTC1_x, (float32_t*) &NTC1_y, A_FSR, B_FSR);
+    IIR(Fluids1_raw, (float32_t*) &Fluids1_x, (float32_t*) &Fluids1_y, A_FSR, B_FSR);
         
-    
-    
     //Decimation
     if(decimationCounter < 5){
       decimationCounter ++;
@@ -229,20 +241,30 @@ void loop() {
     
     
     //Moving_average // Compensates the operations
-    movingAverage(  (float*) &MA_ring_buffer_FSR1,
+    movingAverage(  (float32_t*) &MA_ring_buffer_FSR1,
                     M,
                     RB_i_FSR1,
-                    acc_FSR1,
                     FSR1_y[0],
-                    average_FSR1);
+                    &average_FSR[0],
+                    &min_FSR[0],
+                    &max_FSR[0]);
     
-    movingAverage((float*) &MA_ring_buffer_FSR2, M, RB_i_FSR2, acc_FSR2, FSR2_y[0] * 2, average_FSR2);
-    movingAverage((float*) &MA_ring_buffer_FSR3, M, RB_i_FSR3, acc_FSR3, FSR3_y[0] * 2, average_FSR3);
-    movingAverage((float*) &MA_ring_buffer_FSR4, M, RB_i_FSR4, acc_FSR4, FSR1_y[0] * 2, average_FSR4);
-    movingAverage((float*) &MA_ring_buffer_NTC1, M, RB_i_NTC1, acc_NTC1, NTC1_y[0] * 2, average_NTC1);
-    movingAverage((float*) &MA_ring_buffer_Fluids1, M, RB_i_Fluids1, acc_Fluids1, FSR1_y[0] * 2, average_Fluids1);
+    movingAverage((float32_t*) &MA_ring_buffer_FSR2, M, RB_i_FSR2, FSR2_y[0], &average_FSR[1], &min_FSR[1], &max_FSR[1]);
+    movingAverage((float32_t*) &MA_ring_buffer_FSR3, M, RB_i_FSR3, FSR3_y[0], &average_FSR[2], &min_FSR[2], &max_FSR[2]);
+    movingAverage((float32_t*) &MA_ring_buffer_FSR4, M, RB_i_FSR4, FSR1_y[0], &average_FSR[3], &min_FSR[3], &max_FSR[3]);
+    movingAverage((float32_t*) &MA_ring_buffer_NTC1, M, RB_i_NTC1, NTC1_y[0], &average_NTC1, &min_NTC1, &max_NTC1);
+    movingAverage((float32_t*) &MA_ring_buffer_Fluids1, M, RB_i_Fluids1, FSR1_y[0], &average_Fluids1, &min_Fluids1, &max_Fluids1);
 
-    FSR1_filt = average_FSR1;
+
+    // Get the absolute metrics of the 4 FSR channels
+    arm_mean_f32(average_FSR, 4, &FSR_mean);
+    arm_min_f32(min_FSR, 4, &FSR_min, &idx);
+    arm_max_no_idx_f32(max_FSR, 4, &FSR_max);
+
+    // Voltage to force convertion
+
+
+    FSR1_filt = average_FSR[0];
 
     DAC_o = FSR1_filt * (4095.0f / 3.3f);
     //DAC_o = FSR1_y[0] * (4095.0f / 3.3f);
@@ -255,7 +277,6 @@ void loop() {
     // De-assert DAC
     digitalWrite(sync, HIGH);
     SPI.endTransaction();
-
     
     digitalWrite(DEBUG, LOW);
   }
